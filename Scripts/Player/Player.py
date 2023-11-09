e42dfca06387a610887639e02130e884070c500c
UnitySonic <epichelp276@gmail.com>
2023-11-09 14:50:37 -0500

    Combined JumpStates into one Airborne State. Rewrote Handle Event to only set input flags

--- Scripts/Player/Player.py
+++ Scripts/Player/Player.py
@@ -42,14 +42,21 @@ class Player:
         self.falling = False
         self.scaleFactor = 2.5
         self.yGravity = 1
-        self.jumpHeight = 15
+        self.jumpHeight = 8
         self.yVelocity = 15
         self.currentSprite = 0
         self.originalposY = self.posY
         self.dashCooldown = 1000
         self.timeDashPressed = 0
+        self.isJumping = False
 
         self.state = "idle"
+
+        self.ForwardAirborneSet = ["jumpForward", "fallForward"]
+        self.NeutralAirborneSet = ["jumpNeutral", "fallNeutral"]
+
+        self.refToAirborneSet = None
+        self.elapsedFramesInPhase = 0
 
         self.image = self.idleSheet.subsurface(self.idleSheet.get_clip())
         self.rect = self.image.get_rect()
@@ -70,7 +77,7 @@ class Player:
                              6: (19,305,17,31),
                              7: (18,355,18,29)
                             }
-        
+
         self.runStates =    {0: (19,19,17,29),
                              1: (18,66,18,30),
                              2: (16,113,21,31),
@@ -82,7 +89,7 @@ class Player:
                              8: (16,401,20,31),
                              9: (18,451,18,29)
                             }
-        
+
         self.jumpNeutralStates =    {0: (20,161,14,31),
                                      1: (20,211,14,29),
                                      2: (20,259,14,29),
@@ -92,18 +99,18 @@ class Player:
                                      6: (16,449,19,31),
                                      7: (16,499,18,29)
                                     }
-        
+
         self.fallNeutralStates =    {0: (16,350,19,32),
                                      1: (17,395,18,36),
                                      2: (16,449,19,31),
                                      3: (16,499,18,29)
                                     }
-        
+
         self.jumpForwardStates =    {0: (20,65,15,30),
                                      1: (17,114,20,29),
                                      2: (16,162,16,26),
-                                     3: (13,209,22,27),
-                                     4: (13,153,23,32),
+                                     3: (13,153,23,32),
+                                     4: (13,209,22,27),
                                      5: (17,298,19,37),
                                      6: (17,344,19,39),
                                      7: (19,402,19,30),
@@ -111,20 +118,20 @@ class Player:
                                      9: (18,498,18,30),
                                      10: (20,546,16,30)
                                     }
-        
+
         self.fallforwardStates =    {0: (13,153,23,32),
                                      1: (17,298,19,37),
-                                     2: (17,344,19,39),
-                                     3: (19,402,19,30),
-                                     4: (18,451,19,29)
+                                     #2: (17,344,19,39),
+                                     2: (19,402,19,30),
+                                     3: (18,451,19,29)
                                     }
-        
+
         self.dashStates =           {0: (17,21,29,27),
                                      1: (17,67,29,29),
                                      2: (17,115,29,29),
                                      3: (17,164,29,28)
                                     }
-        
+
         self.stateToSpriteDict =  {"idle": self.idleStates,
                                    "run": self.runStates,
                                    "jumpNeutral": self.jumpNeutralStates,
@@ -153,17 +160,17 @@ class Player:
 
     def getCurrentSpriteWidth(self):
         return self.currentFrameWidth
-    
+
     def getCurrentSpriteHeight(self):
         return self.currentFrameHeight
 
     def getSpriteScale(self):
         return self.scaleFactor
-    
+
     def get_frame(self, frame_set, state):
 
         self.animationTime += 1
-        
+
         if self.animationTime > 3:
             self.frame += 1
             self.animationTime = 0
@@ -193,8 +200,11 @@ class Player:
                 self.stateToSheetDict[state].set_clip(pygame.Rect(clipped_rect))
 
             return clipped_rect
-    
-    def updateSprite(self, stateToUse):
+
+    def updateSprite(self, stateToUse, transition = False):
+
+        if transition:
+            self.frame = 0
 
         self.clip(self.stateToSpriteDict[stateToUse], stateToUse)
         if self.direction == -1:
@@ -226,40 +236,16 @@ class Player:
                 self.leftPressed = False
             elif event.key == pygame.K_RIGHT:
                 self.rightPressed = False
-            if not self.jumping or not self.falling:
-                if self.rightPressed and not self.leftPressed:
-                    self.set_state("run")
-                elif self.leftPressed and not self.rightPressed:
-                    self.set_state("run")
-                elif self.leftPressed and self.rightPressed:
-                    self.set_state("idle")
-                elif not self.leftPressed and not self.rightPressed:
-                    self.set_state("idle")
 
             #jumping
             if event.key == pygame.K_z:
+                print("Jump Released")
                 self.jumpPressed = False
             if not self.jumpPressed:
                 self.falling = True
-                if self.rightPressed and not self.leftPressed:
-                    self.set_state("fallForward")
-                elif not self.rightPressed and self.leftPressed:
-                    self.set_state("fallForward")
-                elif not self.rightPressed and not self.leftPressed:
-                    self.set_state("fallNeutral")
-                elif self.rightPressed and self.leftPressed:
-                    self.set_state("fallNeutral")
-            if self.jumpPressed:
-                self.falling = True
-                if self.rightPressed and not self.leftPressed:
-                    self.set_state("jumpForward")
-                elif not self.rightPressed and self.leftPressed:
-                    self.set_state("jumpForward")
-                elif not self.rightPressed and not self.leftPressed:
-                    self.set_state("jumpNeutral")
-                elif self.rightPressed and self.leftPressed:
-                    self.set_state("jumpNeutral")
-        
+
+
+
         if event.type == pygame.KEYDOWN:
 
             pressed = pygame.key.get_pressed()
@@ -270,53 +256,19 @@ class Player:
             if pressed[pygame.K_RIGHT]:
                 self.rightPressed = True
             if pressed[pygame.K_z]:
+                print("Jump Pressed")
                 self.jumpPressed = True
             if pressed[pygame.K_c]:
                 self.dashPressed = True
                 self.timeDashPressed = pygame.time.get_ticks()
-            if self.rightPressed and not self.leftPressed:
-                self.set_state("run")
-            elif self.leftPressed and not self.rightPressed:
-                self.set_state("run")
-            elif self.leftPressed and self.rightPressed:
-                self.set_state("idle")
-            elif not self.leftPressed and not self.rightPressed:
-                self.set_state("idle")
-            
-            #jumping
-            if self.jumpPressed:
-                if self.rightPressed and not self.leftPressed:
-                    self.jumping = True
-                    self.set_state("jumpForward")
-                elif not self.rightPressed and self.leftPressed:
-                    self.jumping = True
-                    self.set_state("jumpForward")
-                elif not self.rightPressed and not self.leftPressed:
-                    self.jumping = True
-                    self.set_state("jumpNeutral")
-                elif self.rightPressed and self.leftPressed:
-                    self.set_state("jumpNeutral")
-            if self.falling:
-                if self.rightPressed and not self.leftPressed:
-                    self.set_state("fallForward")
-                elif not self.rightPressed and self.leftPressed:
-                    self.set_state("fallForward")
-                elif not self.rightPressed and not self.leftPressed:
-                    self.set_state("fallNeutral")
-                elif self.rightPressed and self.leftPressed:
-                    self.set_state("fallNeutral")
-                    
-            if self.dashPressed:
-                now = pygame.time.get_ticks()
-                if now - self.timeDashPressed >= self.dashCooldown:
-                    self.set_state("dash")
-            
-            
-            
+
+
+
     def update(self):
         self.rect.x = self.posX
         self.rect.y = self.posY
         self.hurtbox.update()
+        self.elapsedFramesInPhase += 1
 
         if self.state == "idle":
             self.idleState()
@@ -332,6 +284,8 @@ class Player:
             self.fallForwardState()
         elif self.state == "dash":
             self.fallForwardState()
+        elif self.state == "air":
+            self.airBorneState()
 
         if self.state == "atk1":
             if self.commitedToAttack != True:
@@ -350,6 +304,72 @@ class Player:
     def idleState(self):
         self.updateSprite("idle")
 
+
+
+        if self.jumpPressed:
+            print("idle")
+            self.isJumping = True
+            self.set_state("air")
+            if self.leftPressed or self.rightPressed:
+                self.refToAirborneSet = self.ForwardAirborneSet
+            else:
+                self.refToAirborneSet = self.NeutralAirborneSet
+
+        if self.leftPressed or self.rightPressed:
+            self.set_state("run")
+
+    def isGrounded(self):
+        collide = pygame.sprite.spritecollide(self.hurtbox, gameLogicFunctions.collisionGroup, False)
+        if collide:
+            return True
+        else:
+            return False
+
+
+
+    def continueGravity(self):
+        maxJumpingFrames = 30
+
+        if self.isJumping == True:
+            self.updateSprite(self.refToAirborneSet[0])
+            self.yVelocity = self.jumpHeight
+
+            if self.jumpPressed == False:
+                self.yVelocity = 0
+                self.isJumping = False
+                self.updateSprite(self.refToAirborneSet[1], True)
+
+            if self.elapsedFramesInPhase < maxJumpingFrames:
+                self.posY -= self.yVelocity
+            elif self.elapsedFramesInPhase >= maxJumpingFrames:
+                self.isJumping = False
+                self.updateSprite(self.refToAirborneSet[1], True)
+
+        else:
+            self.updateSprite(self.refToAirborneSet[1])
+            self.posY -= self.yVelocity
+            self.yVelocity -= self.yGravity
+
+            if self.isGrounded():
+                return False
+        return True
+
+
+
+    def airBorneState(self):
+        if self.continueGravity() == False:
+            self.jumpPressed = False
+            self.set_state("idle")
+        if self.leftPressed:
+            self.direction = -1
+            self.posX += 5 * self.direction
+        elif self.rightPressed:
+            self.direction = 1
+            self.posX += 5 * self.direction
+
+
+
+
     def runState(self):
         if self.leftPressed:
             self.direction = -1
@@ -357,73 +377,17 @@ class Player:
         elif self.rightPressed:
             self.direction = 1
             self.posX += 5 * self.direction
-        self.updateSprite("run")
-
-    def jumpNeutralState(self):
-        if self.jumping:
-            self.updateSprite("jumpNeutral")
-            self.posY -= self.yVelocity
-            self.yVelocity -= self.yGravity
-            if self.yVelocity < 0:
-                self.falling = True
-                self.set_state("fallNeutral")
-            if self.yVelocity < -self.jumpHeight:
-                self.jumping = False
-                self.yVelocity = self.jumpHeight
-                self.set_state("idle")
-                self.idleState()
-                
-    def fallNeutralState(self):
-        if not self.posY == self.originalposY:
-            self.updateSprite("fallNeutral")
-            self.yVelocity += self.yGravity
-            self.posY += self.yVelocity
-        if self.posY == self.originalposY:
-            self.jumping = False
-            self.falling = False
-            self.yVelocity = self.jumpHeight
+        else:
             self.set_state("idle")
-            self.idleState()
-            
-    def jumpForwardState(self):
-        if self.jumping:
-            self.updateSprite("jumpForward")
-            self.posY -= self.yVelocity
-            self.yVelocity -= self.yGravity
-            if self.leftPressed:
-                self.direction = -1
-                self.posX += 5 * self.direction
-            elif self.rightPressed:
-                self.direction = 1
-                self.posX += 5 * self.direction
-            if self.yVelocity < 0:
-                self.falling = True
-                self.set_state("fallForward")
-            if self.yVelocity < -self.jumpHeight:
-                self.jumping = False
-                self.yVelocity = self.jumpHeight
-                self.set_state("run")
-                self.runState()
-                
-    def fallForwardState(self):
-        if not self.posY == self.originalposY:
-            self.updateSprite("fallForward")
-            self.yVelocity += self.yGravity
-            self.posY += self.yVelocity
-            if self.leftPressed:
-                self.direction = -1
-                self.posX += 5 * self.direction
-            elif self.rightPressed:
-                self.direction = 1
-                self.posX += 5 * self.direction
-        if self.posY == self.originalposY:
-            self.jumping = False
-            self.falling = False
-            self.yVelocity = self.jumpHeight
-            self.set_state("run")
-            self.runState()
-            
+        self.updateSprite("run")
+        if self.jumpPressed:
+            self.isJumping = True
+            self.set_state("air")
+            if self.leftPressed or self.rightPressed:
+                self.refToAirborneSet = self.ForwardAirborneSet
+            else:
+                self.refToAirborneSet = self.NeutralAirborneSet
+
     def dashState(self):
         self.posX += 15 * self.direction
         self.updateSprite("dash")
-        
